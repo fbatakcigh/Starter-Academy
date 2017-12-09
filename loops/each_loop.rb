48093ed75d2552284402d77b423edea21429496f
Gurban H <ghaydarov@gmail.com>
2017-12-09 10:20:00 -0500

    each loop

---
+++ loops/each_loop.rb
@@ -0,0 +1,13 @@
+arr = ['a', 'b', 'c', 'b', 'd', 't']
+
+# Each loop
+arr.each do |letter|
+	puts "This is letter #{letter}"
+end
+
+# Each loop with index
+arr.each_with_index do |letter, index|
+	puts "#{index + 1}. This is letter #{letter}"
+end
+
+
